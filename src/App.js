90c2d5de1aa81990b283f31aac0069e42ba2ec0e
anurag <mishranaman123@gmail.com>
2022-06-26 12:21:12 +0530

    feat: React Router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home'; //configuring route
+// import About from './components/About'; //configuring route
+import Navbar from './components/Navbar'; //link
+import OrderSummary from './components/OrderSummary'; //progarmmatically navigation
+import NoMatch from './components/NoMatch'; // no match routes
+import Products from './components/Products';
+import FeaturedProduct from './components/FeaturedProduct';
+import NewProduct from './components/NewProduct';
+import Users from './components/Users';
+import UserDetails from './components/UserDetails';
+import Admin from './components/Admin';
+import Profile from './components/Profile';
+import { AuthProvider } from './components/auth';
+import Login from './components/Login';
+
+const LazyAbout = React.lazy(() => import('./components/About')) // Lazy Loading
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<AuthProvider>
+			<Navbar />
+			<Routes>
+				<Route path='/' element={<Home/>} />
+				<Route path='about' element={
+					<React.Suspense fallback='Loading'>
+						<LazyAbout/>
+					</React.Suspense>} />
+				<Route path='order-summary' element={<OrderSummary/>} />
+				<Route path='products' element={<Products />} >
+					<Route index element={<FeaturedProduct/>} />
+					<Route path='featured' element={<FeaturedProduct/>} />
+					<Route path='new' element={<NewProduct/>} />
+				</Route>
+				<Route path='users' element={<Users/>} >
+					<Route path=':userId' element={<UserDetails/>} />
+					<Route path='admin' element={<Admin/>} />
+				</Route>
+				<Route path='profile' element = {<Profile />}/>
+				<Route path='login' element={<Login />}/>
+				<Route path='*' element={<NoMatch/>} />
+			</Routes>	
+		</AuthProvider>
+	);
 }
 
 export default App;
